7b95cc6b2367d4092c187face80c704da4818e50
Nicholas Major <nick@nmajor.com>
2015-01-01 01:54:42 -0800

    css changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     hh, mm = mm.divmod(60)
     dd, hh = hh.divmod(24)
 
-    "<span class=\"day-words\">#{dd} days</span class=\"hour-words\"><span>#{hh} hours</span><span class=\"minute-words\">#{mm} minutes</span>".html_safe
+    "<span class=\"day-words\">#{dd} days,</span class=\"hour-words\"><span>#{hh} hours,</span><span class=\"minute-words\">#{mm} minutes</span>".html_safe
   end
 
   def minutes_to_hours mm
