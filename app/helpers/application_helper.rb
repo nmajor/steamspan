d3390a4184d93990c9d2383ba80a2c627b8ea9d6
Nicholas Major <nick@nmajor.com>
2015-01-01 01:46:27 -0800

    wording changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     hh, mm = mm.divmod(60)
     dd, hh = hh.divmod(24)
 
-    "<span class=\"day-words\">#{dd} days</span class=\"hour-words\"><span>#{hh} Hours</span><span class=\"minute-words\">#{mm} Minutes</span>".html_safe
+    "<span class=\"day-words\">#{dd} days</span class=\"hour-words\"><span>#{hh} hours</span><span class=\"minute-words\">#{mm} minutes</span>".html_safe
+  end
+
+  def minutes_to_hours mm
+    mm / 60
   end
 end
