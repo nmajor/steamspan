091c13d12f7a2fbc47c5599869ad9489fd703722
Nicholas Major <nick@nmajor.com>
2015-12-12 18:28:03 +0700

    Fixing unicorn worker issue

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
 app_dir = File.expand_path("../..", __FILE__)
 working_directory app_dir
 
-pid "#{app_dir}/tmp/unicorn.pid"
+pid "/tmp/unicorn.pid"
 
 # stderr_path "#{app_dir}/log/unicorn.stderr.log"
 # stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
-worker_processes ENV["UNICORN_WORKERS"]
+worker_processes ENV["UNICORN_WORKERS"] ? ENV["UNICORN_WORKERS"].to_i : 1
 listen "/tmp/unicorn.sock", :backlog => 64
 timeout 30
