9218139ce37d36436d4d8930b916facdae411c46
Nicholas Major <nick@nmajor.com>
2016-05-19 23:51:31 +0700

    Fixed a bad path to nginx errors, and turned on unicorn logging.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,8 +3,8 @@ working_directory app_dir
 
 pid "/tmp/unicorn.pid"
 
-# stderr_path "#{app_dir}/log/unicorn.stderr.log"
-# stdout_path "#{app_dir}/log/unicorn.stdout.log"
+stderr_path "#{app_dir}/log/unicorn.stderr.log"
+stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
 worker_processes ENV["UNICORN_WORKERS"] ? ENV["UNICORN_WORKERS"].to_i : 1
 listen "/tmp/unicorn.sock", :backlog => 64
